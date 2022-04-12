1dad4766d91cafcd4d56f889c32388ef982ec320
Rosco Kalis <roscokalis@gmail.com>
2022-04-12 10:15:35 +0200

    Add revoke.cash' useEthereum hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { EthereumProvider } from '../utils/hooks/useEthereum'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <EthereumProvider>
+      <Component {...pageProps} />
+    </EthereumProvider>
+  )
 }
 
 export default MyApp
