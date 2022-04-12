359fda77e785a5c3ac3b72b0abed01ac5073d10a
Rosco Kalis <roscokalis@gmail.com>
2022-04-12 13:18:32 +0200

    Add next-lint

--- components/CountTable.tsx
+++ components/CountTable.tsx
@@ -22,8 +22,10 @@ export const CountTable = ({ title, counts, currentSigners }: Props) => {
         <tbody>
 
         {Object.entries(counts).sort((a, b) => b[1] - a[1]).map(([signer, count]) => (
-          <tr>
-            <td className={`border p-1 decoration-red-500 ${currentSigners.includes(signer) ? '' : 'line-through'}`}><AddressDisplay address={signer} /></td>
+          <tr key={signer}>
+            <td className={`border p-1 decoration-red-500 ${currentSigners.includes(signer) ? '' : 'line-through'}`}>
+              <AddressDisplay address={signer} />
+            </td>
             <td className="border p-1">{count}</td>
           </tr>
         ))}
